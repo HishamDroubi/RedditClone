141d58d55de95882b1bd726aa4cac7c581ae1cd5
HishamDroubi <hisham2001.droubi@gmail.com>
2022-09-14 14:16:25 +0300

    Initial Commit For Reddit Clone Spring Boot Project

---
+++ springit/src/main/java/com/vega/springit/SpringitApplication.java
@@ -0,0 +1,13 @@
+package com.vega.springit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringitApplication.class, args);
+	}
+
+}
---
+++ springit/src/test/java/com/vega/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vega.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
